a70a3d19db26e5e8ec26ef1f783297152964ed3c
John Nieri <niericentral@gmail.com>
2013-01-16 01:32:06 +0900

    Update to setup.py: match new import style (py_modules instead of packages) description url

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='simpletree',
     version='0.1.3',
-    packages=['simpletree'],
-    url='',
-    license='MIT',
-    author='KobeJohn',
-    author_email='niericentral@gmail.com',
-    description='Tree and Nodes using only one class. Arbitrary data items per node.'
+    name = 'simpletree',
+    py_modules = ['simpletree'],
+    url = 'http://github.com/kobejohn/simpletree',
+    license = 'MIT',
+    author = 'KobeJohn',
+    author_email = 'niericentral@gmail.com',
+    description = 'Class with unified tree and node behavior. Each node has custom data as attributes.'
 )
