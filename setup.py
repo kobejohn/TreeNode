3704f68e1068e855e2042790d7c99e7a27cbaf4f
John Nieri <niericentral@gmail.com>
2013-01-16 12:51:47 +0900

    update setup.py for pypi

--- setup.py
+++ setup.py
@@ -3,10 +3,18 @@ from distutils.core import setup
 setup(
     name = 'treenode',
     version = '0.1.5',
-    py_modules = ['treenode'],
     url = 'http://github.com/kobejohn/simpletree',
+    py_modules = ['treenode'],
+    author_email = 'niericentral@gmail.com',
+    description = 'Class with unified tree and node behavior. Each node has custom data as attributes.',
     license = 'MIT',
     author = 'KobeJohn',
-    author_email = 'niericentral@gmail.com',
-    description = 'Class with unified tree and node behavior. Each node has custom data as attributes.'
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2'
+    ]
 )
